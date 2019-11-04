fa2eec8e10c9c6473820448fe033c97cda820035
Caro Guerrero <caroguerrerosilvera@gmail.com>
2019-11-03 23:08:49 -0500

    Add sidebar component

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,72 +1,6 @@
 <template>
   <div class="page-container">
-    <!-- sidebar menu area start -->
-    <div class="sidebar-menu">
-        <div class="sidebar-header">
-            <div class="logo">
-                <a href="index.html">
-                    <!-- <img src="assets/images/icon/logo.png" alt="InvenDolls"> -->
-                    IvenDolls
-                </a>
-            </div>
-        </div>
-        <div class="main-menu">
-            <div class="menu-inner">
-                <nav>
-                    <ul class="metismenu" id="menu">
-                        <li class="active">
-                            <a href="javascript:void(0)" aria-expanded="true">
-                                <i class="ti-dashboard"></i>
-                                <span>Inicio</span>
-                            </a>
-                            <ul class="collapse">
-                                <li class="active">
-                                    <a href="/inicio.html">
-                                        Floresta
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="/inicio.html">
-                                        Santa Lucía
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="/ventas.html"><i
-                                    class="ti-receipt"></i><span>Ventas
-                                </span></a>
-                        </li>
-                        <li>
-                            <a href="/productos.html"><i
-                                    class="ti-package"></i><span>Productos</span></a>
-                        </li>
-                        <li>
-                            <a href="/clientes.html"><i class="ti-user"></i><span>Clientes</span></a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0)" aria-expanded="true"><i
-                                    class="ti-pie-chart"></i><span>Reportes</span></a>
-                            <ul class="collapse">
-                                <li><a href="/reportes-ventas.html">Por Ventas</a></li>
-                                <li><a href="/reportes-inventario.html">Por Inventario</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0)" aria-expanded="true"><i class="ti-settings"></i>
-                                <span>Configuración</span></a>
-                            <ul class="collapse">
-                                <li><a href="/usuarios.html">Usuarios</a></li>
-                                <li><a href="/roles.html">Roles</a></li>
-                                <li><a href="/almacenes.html">Almacenes</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </div>
-    <!-- sidebar menu area end -->
+    <SideBar :isVisible="false" />
     <!-- main content area start -->
     <div class="main-content">
         <!-- header area start -->
@@ -136,7 +70,12 @@
 </template>
 
 <script>
+import SideBar from '../components/SideBar'
+
 export default {
-  name: 'MainLayout'
+  name: 'MainLayout',
+  components: {
+    SideBar
+  }
 }
 </script>
